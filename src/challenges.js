9b25c6021a6ffeb71f9fd0eb882929de291ed1a9
Lucas Pedro <rodrigerioo@gmail.com>
2022-06-27 17:45:42 -0300

    exercicio 05 feito

--- src/challenges.js
+++ src/challenges.js
@@ -4,37 +4,44 @@ const um = true;
 const dois = true;
 const tres = false;
 
-function compareTrue (param1, param2) {
-if (param1 === true && param2 === true) { 
-  return true;  
+function compareTrue(param1, param2) {
+  if (param1 === true && param2 === true) {
+  return true;
 } else {
   return false;
 }
 }
-compareTrue(um, dois)
+compareTrue(um, dois);
 
 // Desafio 2
 
 function calcArea(base, height) {
 
-let valorArea = (base * height) / 2
-return valorArea
+  let valorArea = (base * height) / 2;
+  return valorArea;
 }
 
 // Desafio 3
 function splitSentence(param1) {
- let resultado = param1.split(', ')
-return resultado
+  let resultado = param1.split('');
+  return resultado;
 }
 
 // Desafio 4
-function concatName() {
-  // seu código aqui
+function concatName(array) {
+let teste = array
+
+return teste.length, teste[0]
 }
 
+
 // Desafio 5
-function footballPoints() {
-  // seu código aqui
+function footballPoints(w, t) {
+let win = 3
+let tie = 1
+
+let quantidadePontos = win * w + tie * t 
+return quantidadePontos
 }
 
 // Desafio 6
