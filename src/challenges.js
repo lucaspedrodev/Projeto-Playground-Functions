06a76833f8ccb274076f1dbfba7be9c034956dda
Lucas Pedro <rodrigerioo@gmail.com>
2023-05-22 14:14:37 -0300

    requisito

--- src/challenges.js
+++ src/challenges.js
@@ -6,17 +6,15 @@ const tres = false;
 
 function compareTrue(param1, param2) {
   if (param1 === true && param2 === true) {
-  return true;
-} else {
+    return true;
+  }
   return false;
-}
 }
 compareTrue(um, dois);
 
 // Desafio 2
 
 function calcArea(base, height) {
-
   let valorArea = (base * height) / 2;
   return valorArea;
 }
@@ -29,34 +27,32 @@ function splitSentence(param1) {
 
 // Desafio 4
 function concatName(array) {
-let teste = array[array.length-1] + ', ' + array[0];
-return teste
+  let teste = `${array[array.length - 1]}, ${array[0]}`;
+  return teste;
 }
-
 
 // Desafio 5 FEITO
 function footballPoints(w, t) {
-let win = 3
-let tie = 1
-                                            
-let quantidadePontos = win * w + tie * t 
-return quantidadePontos
+  let win = 3;
+  let tie = 1;
+
+  let quantidadePontos = win * w + tie * t;
+  return quantidadePontos;
 }
 
 // Desafio 6
 function highestCount(arrayDeNumeros) {
- let r = 0
+  let r = 0;
 
- let maiorNumero = Math.max.apply(null, arrayDeNumeros)
+  let maiorNumero = Math.max.apply(null, arrayDeNumeros);
 
- for (let index = 0; index < arrayDeNumeros.length; index += 1) {
-  if (maiorNumero === arrayDeNumeros[index]){
-    r++
+  for (let index = 0; index < arrayDeNumeros.length; index += 1) {
+    if (maiorNumero === arrayDeNumeros[index]) {
+      r++;
+    }
   }
-  }
-  return r++
-  }
- 
+  return r++;
+}
 
 // Desafio 7
 function catAndMouse(mouse, cat1, cat2) {
@@ -64,39 +60,32 @@ function catAndMouse(mouse, cat1, cat2) {
   let poscat2 = Math.abs(mouse - cat2);
 
   if (poscat1 < poscat2) {
-    return 'cat1'
-  } else if (poscat2 < poscat1) {
-    return 'cat2'
-  } else if (poscat1 === poscat2) {
-    return 'os gatos trombam e o rato foge'
+    return 'cat1';
+  } if (poscat2 < poscat1) {
+    return 'cat2';
+  } if (poscat1 === poscat2) {
+    return 'os gatos trombam e o rato foge';
   }
-
-  
 }
 
 // Desafio 8
 function fizzBuzz(array1) {
   let resultado = [];
 
-  for (let index = 0; index < array1.length; index += 1){
-
-  if (array1[index] % 5 === 0 && array1[index] % 3 === 0) {
-    resultado.push('fizzBuzz')
-
-  } else if (array1[index] % 3 === 0) {
-    resultado.push ('fizz')
-
-  } else if (array1[index] % 5 === 0) {
-    resultado.push('buzz')
-
-
-  } else if (array1[index] % 5 && 3 !== 0){
-    resultado.push('bug!')
-  } 
+  for (let index = 0; index < array1.length; index += 1) {
+    if (array1[index] % 5 === 0 && array1[index] % 3 === 0) {
+      resultado.push('fizzBuzz');
+    } else if (array1[index] % 3 === 0) {
+      resultado.push('fizz');
+    } else if (array1[index] % 5 === 0) {
+      resultado.push('buzz');
+    } else if (array1[index] % 5 && 3 !== 0) {
+      resultado.push('bug!');
+    }
+  }
+  return resultado;
 }
-  return resultado
-}
-
+console.log(fizzBuzz(3, 5));
 // Desafio 9
 function encode() {
   // seu código aqui
